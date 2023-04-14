cdbfb02fd7aa9ed52be48f0e4b00773ff6253c4b
Rebeca <rehdomingos123@gmail.com>
2023-04-14 13:18:20 -0300

    add user schema

---
+++ src/models/user.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        id: { type: String },
+        username: { type: String, required: true },
+        password: { type: String, required: true }
+    },
+    {
+        versionKey: false
+    }
+);
+
+const User = mongoose.model("users", UserSchema);
+
+export default User;
