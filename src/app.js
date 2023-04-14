e7c99ee6ff53695d7fbea1a42ef962bdc648f875
Rebeca <rehdomingos123@gmail.com>
2023-04-14 13:19:18 -0300

    add routes to app file

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dbConnect from './config/dbConnect.js'
+import routes from './routes/index.js'
 
 dbConnect();
 
 const app = express();
 app.use(express.json());
+routes(app);
 
 export default app
