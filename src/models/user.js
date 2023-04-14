bbb33781f74925e6575e08aec288e19982b48c9e
Rebeca <rehdomingos123@gmail.com>
2023-04-14 14:02:15 -0300

    fix user model

--- src/models/user.js
+++ src/models/user.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema(
     {
         id: { type: String },
-        username: { type: String, required: true },
+        username: { type: String, required: true, unique: true },
         password: { type: String, required: true }
     },
     {
