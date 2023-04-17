0b6dc8acf528d3e7a886effc52e645624a510139
Rebeca <rehdomingos123@gmail.com>
2023-04-16 21:36:00 -0300

    add cors to app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import dbConnect from './config/dbConnect.js'
-import routes from './routes/index.js'
+import dbConnect from './config/dbConnect.js';
+import routes from './routes/index.js';
+import cors from 'cors';
 
 dbConnect();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 routes(app);
 
