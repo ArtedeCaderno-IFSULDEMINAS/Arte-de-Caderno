b0795907d716eaff9df52fca22672cf992866ec7
Rebeca <rehdomingos123@gmail.com>
2023-05-31 19:53:06 -0300

    add list school by city router

--- src/routes/schoolRoute.js
+++ src/routes/schoolRoute.js
@@ -4,6 +4,7 @@ import SchoolController from "../controllers/SchoolController.js";
 const schoolRouter = express.Router();
 
 schoolRouter.get("/school", SchoolController.listSchool)
-            .post("/school", SchoolController.insertSchool);
+            .post("/school", SchoolController.insertSchool)
+            .post("/school/city", SchoolController.listSchoolByCity);
 
 export default schoolRouter;
