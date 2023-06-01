49ee5e2f9666fc659f1ee1b679a581e2a332aed6
Rebeca <rehdomingos123@gmail.com>
2023-05-31 21:00:57 -0300

    add list uf, list city and list school bt city routes

--- src/routes/schoolRoute.js
+++ src/routes/schoolRoute.js
@@ -5,6 +5,8 @@ const schoolRouter = express.Router();
 
 schoolRouter.get("/school", SchoolController.listSchool)
             .post("/school", SchoolController.insertSchool)
-            .post("/school/city", SchoolController.listSchoolByCity);
+            .post("/school/city", SchoolController.listCitiesByUf)
+            .get("/school/uf", SchoolController.listUfs)
+            .post("/school/listByCity", SchoolController.listSchoolByCity);
 
 export default schoolRouter;
