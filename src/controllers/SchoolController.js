51e69e3b5af54588c7d8ff429ac32df30ab3364a
Rebeca <rehdomingos123@gmail.com>
2023-05-31 21:00:04 -0300

    add list uf, list city and list school bt city controllers

--- src/controllers/SchoolController.js
+++ src/controllers/SchoolController.js
@@ -51,6 +51,34 @@ class SchoolController {
             }
         }
 
+        listUfs = async (req, res) => {
+            try{
+                const ufs = await School.find().distinct('uf');
+                res.status(200).json(ufs);
+            }
+            catch(err){
+                res.status(500).json({message: err.message});
+            }
+        }
+
+        listCitiesByUf = async (req, res) => {
+            const {uf} = req.body;
+            if(uf === null){
+                return res.status(400).json({message: 'Uf is required'});
+            }
+            if(uf.length !== 2){
+                return res.status(400).json({message: 'Uf must have 2 characters'});
+            }
+            try{
+                const cities = await School.find({uf: uf}).distinct('city');
+                res.status(200).json(cities);
+            }
+            catch(err){
+                res.status(500).json({message: err.message});
+            }
+        }
+
+
 
 }
 export default new SchoolController;
