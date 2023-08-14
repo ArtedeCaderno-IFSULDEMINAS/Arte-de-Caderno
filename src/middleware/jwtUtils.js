c7fa5d83732eddcdacb2348a6ee44e8cde95cdfb
Rebeca <rehdomingos123@gmail.com>
2023-08-14 20:13:18 -0300

    add handler error 404

--- src/middleware/jwtUtils.js
+++ src/middleware/jwtUtils.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
 const generateToken = (payload) => {
     let secret = process.env.secret;
-    const token = jwt.sign(payload,secret, { expiresIn: 86400 });
+    const token = jwt.sign(payload,secret, { expiresIn: 432000 });
     return token;
 };
 
