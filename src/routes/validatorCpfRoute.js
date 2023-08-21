b2cce1f1f16898a67cef35e8e03f5ae749c0955f
Rebeca <rehdomingos123@gmail.com>
2023-08-20 22:12:14 -0300

    add error treatment

--- src/routes/validatorCpfRoute.js
+++ src/routes/validatorCpfRoute.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import Login from '../models/login';
 import validarCpf from 'validar-cpf';
 
 const validatorCpfRoute = express.Router();
 
 validatorCpfRoute.get('/cpf/:cpf', (req, res) => {
     const { cpf } = req.params;
+
+    let userExists = Login.find({username: cpf});
+    
+    if(userExists !== null){
+        return res.status(400).json({message: 'User already exists'});
+    }
 
     const valid = validarCpf(cpf);
 
