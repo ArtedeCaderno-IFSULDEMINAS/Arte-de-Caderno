c6a8789c4c0c0ddbfdeaa07c8e54088a03d20df2
Rebeca <rehdomingos123@gmail.com>
2023-09-19 21:35:11 -0300

    add distribute draws route

--- src/routes/drawRoute.js
+++ src/routes/drawRoute.js
@@ -8,10 +8,10 @@ drawRoute.use(authenticateTokenJwt);
 drawRoute.get('/draw/all', DrawController.listAllDraws)
         .get('/draw/:id', DrawController.getDrawById)
         .get('/draw/student/:id', DrawController.getDrawByStudent)
-        .get('/draw/allDesclassified', DrawController.listDesclassifiedDraws)
         .post('/draw/category', DrawController.getDrawByCategory)
         .post('/draw', DrawController.insertDraw)
         .post('/draw/score/:id', DrawController.insertScoreDraw)
-        .post('/draw/desclassified/:id', DrawController.desclassifiedDraw);
+        .post('/draw/desclassified/:id', DrawController.desclassifiedDraw)
+        .post('/draw/distribute', DrawController.distributeDraws);
 
 export default drawRoute;
