083a9ebdc0d52dda1f9bcf1da817b19e983ee76e
Rebeca <rehdomingos123@gmail.com>
2023-09-28 14:20:59 -0300

    remove server variable

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 const port = process.env.PORT || 3000;
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server is running on port ${port}.`);
 });
-
-export default server;
