806cc4d86aff60e5d1c6f7bdad20a3fabf52cee5
Beatriz Neaime <beatriz.neaime@alunos.ifsuldeminas.edu.br>
2023-11-27 15:34:16 -0300

    Refactor: about page
    Development Team inside a slider in about page

--- artedecaderno.front/src/components/navbar/index.jsx
+++ artedecaderno.front/src/components/navbar/index.jsx
@@ -1,26 +1,14 @@
-import { links, loggedLinks } from "src/router/links";
+import { links } from "src/router/links";
 import { MenuContainer, NavDrop, NavLink, NavbarRow } from "./components/style";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { faBars} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useMediaQuery } from "src/hooks/useMediaQuery";
 import { useState } from "react";
-import Cookies from "js-cookie";
-import { Row } from "src/styles/sharedStyles";
-import { icons } from "src/styles/icons";
 
 const Navbar = ({ currentPage }) => {
-  const desktop = useMediaQuery("(min-width: 770px)");
+  const desktop = useMediaQuery("(min-width: 768px)");
   const [sidebar, setSidebar] = useState(false);
-  const isLogged = Cookies.get("isLogged");
-  const navigate = useNavigate()
-
-  const logout = () => {
-    Cookies.remove("user");
-    Cookies.remove("token");
-    Cookies.remove("isLogged");
-    Cookies.remove("accessType");
-    navigate("/login")
-  };
 
   return (
     <NavbarRow
@@ -29,7 +17,7 @@ const Navbar = ({ currentPage }) => {
       }}
     >
       <MenuContainer width={"30%"}>
-        <Link to={"/dashboard"}>
+        <Link to={"/"}>
           <img
             src={require("src/assets/img/logos/logo.png")}
             alt=""
@@ -42,14 +30,14 @@ const Navbar = ({ currentPage }) => {
       {desktop && (
         <>
           <MenuContainer
-            width={isLogged ? "70%" : "40%"}
+            width={"40%"}
             style={{ alignItems: "end", height: "60px" }}
           >
             {links.map((link) => {
               return (
                 <Link
-                  to={link.path}
-                  key={link}
+                to={link.path}
+                key={link}
                   style={{
                     color: "black",
                     textDecoration:
@@ -61,31 +49,32 @@ const Navbar = ({ currentPage }) => {
                 </Link>
               );
             })}
-            {isLogged &&
-              loggedLinks.map((link, index) => {
-                return (
-                  <Link
-                    to={link.path}
-                    key={link}
-                    style={{
-                      color: "black",
-                      textDecoration:
-                        link.text === currentPage ? "underline" : "none",
-                      opacity: link.text === currentPage ? 1 : 0.7,
-                    }}
-                  >
-                    <NavLink>{link.text}</NavLink>
-                  </Link>
-                );
-              })}
-            {isLogged && <NavLink onClick={logout} style={{color: "red"}} >Sair</NavLink>}
+          </MenuContainer>
+          <MenuContainer
+            width={"30%"}
+            style={{ justifyContent: "center", gap: "5px" }}
+          >
+            {/* <FontAwesomeIcon icon={faCircleUser} className="fa-xl" />
+            <Link
+              to={"/login"}
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              <NavLink>Entrar</NavLink>
+            </Link>
+            /
+            <Link
+              to={"/cadastrar"}
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              <NavLink>Cadastrar</NavLink>
+            </Link> */}
           </MenuContainer>
         </>
       )}
 
       {!desktop && (
         <FontAwesomeIcon
-          icon={icons.hamburger}
+          icon={faBars}
           style={{
             position: "fixed",
             top: "30px",
@@ -100,81 +89,21 @@ const Navbar = ({ currentPage }) => {
         <NavDrop onBlur={() => setSidebar(false)}>
           {links.map((link) => {
             return (
-              <Row style={{ padding: "0.5rem" }}>
-                <Link
-                  key={link}
-                  to={link.path}
-                  style={{
-                    color: "black",
-                    textDecoration:
-                      link.text === currentPage ? "underline" : "none",
-                    opacity: link.text === currentPage ? 1 : 0.7,
-                  }}
-                >
-                  <NavLink>{link.text}</NavLink>
-                </Link>
-              </Row>
-            );
-          })}
-          {!isLogged && (
-            <Row style={{ padding: "0.5rem" }}>
               <Link
-                to={"/login"}
+                key={link}
+                to={link.path}
                 style={{
                   color: "black",
-                  textDecoration: "none",
-                  opacity: 0.7,
+                  textDecoration:
+                    link.text === currentPage ? "underline" : "none",
+                  opacity: link.text === currentPage ? 1 : 0.7,
                 }}
               >
-                <NavLink>Entrar</NavLink>
+                <NavLink>{link.text}</NavLink>
               </Link>
-            </Row>
-          )}
-          {isLogged &&
-            loggedLinks.map((link, index) => {
-              return (
-                <Row style={{ padding: "1rem" }}>
-                  <Link
-                    to={link.path}
-                    key={link}
-                    style={{
-                      color: "black",
-                      textDecoration:
-                        link.text === currentPage ? "underline" : "none",
-                      opacity: link.text === currentPage ? 1 : 0.7,
-                    }}
-                  >
-                    <NavLink>{link.text}</NavLink>
-                  </Link>
-                </Row>
-              );
-            })}
-          <Row style={{ padding: "1rem" }}>
-            {isLogged && <NavLink onClick={logout}>Sair</NavLink>}
-          </Row>
+            );
+          })}
         </NavDrop>
-      )}
-
-      {desktop && !isLogged && (
-        <MenuContainer
-          width={"30%"}
-          style={{ justifyContent: "center", gap: "5px" }}
-        >
-          <FontAwesomeIcon icon={icons.user} className="fa-xl" />
-          <Link
-            to={"/login"}
-            style={{ textDecoration: "none", color: "black" }}
-          >
-            <NavLink>Entrar</NavLink>
-          </Link>
-          /
-          <Link
-            to={"/cadastrar"}
-            style={{ textDecoration: "none", color: "black" }}
-          >
-            <NavLink>Cadastrar</NavLink>
-          </Link>
-        </MenuContainer>
       )}
     </NavbarRow>
   );
